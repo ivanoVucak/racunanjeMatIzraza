5517cc69460cacdf6792ec3b9073cc20c13dc78e
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-11-08 09:15:48 +0100

    Add files via upload

---
+++ deklinirianjeDefiniranje/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deklinirianjeDefiniranje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0, c = 0, d = 0;
+
+            Console.Write("Unsei vrijednost od a --> ");
+            a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Unsei vrijednost od b --> ");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Unsei vrijednost od c --> ");
+            c = Convert.ToDouble(Console.ReadLine()); 
+            
+            Console.Write("Unsei vrijednost od d --> ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            double rez = a*a + b*b + c*c + d*d;
+            double rez2 = (a + b) * (a + b) + (c * d) * (c * d);
+            double x = rez / rez2;
+
+            Console.WriteLine("Po matematickom izrazu rezultat je --> " + x);
+
+
+            Console.ReadKey();
+        }
+    }
+}
